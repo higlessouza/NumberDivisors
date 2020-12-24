968ce52e7edd1e13c7c90a0dd2e889b8c9788280
Higles Souza <43656269+higlessouza@users.noreply.github.com>
2020-12-24 12:48:18 -0300

    Atualização dos teste unitarios
    Implementação de testes na classe Number Model

---
+++ Prova_Localiza.Tests/NumberModelTest.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Prova_Localiza.Models;
+
+namespace Prova_Localiza.Tests
+{
+    [TestClass]
+    public class NumberModelTest
+    {
+
+        /// <summary>
+        /// Testa se o metodo consegue identificar os numeros primos
+        /// </summary>
+        [TestMethod]
+        public void IsPrimeNumberTest()
+        {
+            NumberModel number = new NumberModel(7);
+            Assert.IsTrue(number.PrimeNumber);
+
+            number = new NumberModel(5);
+            Assert.IsTrue(number.PrimeNumber);
+
+            number = new NumberModel(13);
+            Assert.IsTrue(number.PrimeNumber);
+
+            number = new NumberModel(17);
+            Assert.IsTrue(number.PrimeNumber);
+        }
+
+        /// <summary>
+        /// Testa se o metodo consegue identificar que os numeros não são primos
+        /// </summary>
+        [TestMethod]
+        public void NotPrimeNumberTest()
+        {
+            NumberModel number = new NumberModel(6);
+            Assert.IsFalse(number.PrimeNumber);
+
+            number = new NumberModel(9);
+            Assert.IsFalse(number.PrimeNumber);
+
+            number = new NumberModel(10);
+            Assert.IsFalse(number.PrimeNumber);
+
+            number = new NumberModel(10);
+            Assert.IsFalse(number.PrimeNumber);
+        }
+    }
+}
